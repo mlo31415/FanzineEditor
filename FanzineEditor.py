5a27cbd28526d759ca835b42af71a53ec0b71bbe
mlo31415 <mlo@baskerville.org>
2023-09-16 17:52:11 -0400

    Suppress the row labels

--- FanzineEditor.py
+++ FanzineEditor.py
@@ -128,6 +128,7 @@ class FanzineEditor(FanzineGrid):
         fanzinesList=GetFanzineList()
         fanzinesList.sort(key=lambda name: name.casefold())
         self.Datasource.FanzineList=fanzinesList
+        self._dataGrid.HideRowLabels()
 
         # Position the window on the screen it was on before
         tlwp=Settings().Get("Top Level Window Position")
