d4e34c03a8c2221b4f8bdf8cd587e219af3818bc
mlo31415 <mlo@baskerville.org>
2023-11-15 19:40:42 -0500

    Do a proper job of appending the new coldefs

--- FanzineIndexPageEdit.py
+++ FanzineIndexPageEdit.py
@@ -1373,7 +1373,8 @@ class FanzineIndexPage(GridDataSource):
         self._colDefs=ColNamesToColDefs(headers)
         # Column #1 is always a link to the fanzine, and we split this into two parts, the URL and the display text
         # We prepend a URL column before the Issue column. This will hold the filename which is the URL for the link
-        self._colDefs=ColDefinitionsList([ColDefinition("URL", 100, "URL", IsEditable.No)]).append(self._colDefs)
+        self._colDefs=ColDefinitionsList([ColDefinition("URL", 100, "URL", IsEditable.No)])
+        self._colDefs.append(ColNamesToColDefs(headers))
 
         rows: list[list[str]]=[]
         if len(theRows) > 1:
