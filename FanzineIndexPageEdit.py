926387207c246865783c161ca486b6dbd45f28a9
mlo31415 <mlo@baskerville.org>
2024-03-11 18:11:42 -0400

    We do not need the result of this erfror mesdsage dialog

--- FanzineIndexPageEdit.py
+++ FanzineIndexPageEdit.py
@@ -565,7 +565,7 @@ class FanzineIndexPageWindow(FanzineIndexPageEditGen):
 
             if failure:
                 dlg=wx.MessageDialog(self, f"Upload failed")
-                result=dlg.ShowModal()
+                dlg.ShowModal()
 
             # If this is a new fanzine, it needs to be in a new directory.  Check it.
             if self.IsNewDirectory:
