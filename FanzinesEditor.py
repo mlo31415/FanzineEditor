c6daaa314ad20c1223343385c260f071bab3e822
mlo31415 <mlo@baskerville.org>
2024-09-12 16:51:37 -0400

    Add comment

--- FanzinesEditor.py
+++ FanzinesEditor.py
@@ -221,7 +221,7 @@ def GetFanzinesList() -> list[ClassicFanzinesLine]|None:
         # Column 6: Flag
         # '<td sorttable_customkey="zzzz"><br/>'
         if len(row) < 7:
-            row.append("")
+            row.append("")      # This column is optional, but it's easier tpo process if we add it on as blank
         if row[6] != "":
             m=re.search(r'<td\s*sorttable_customkey=[\'\"](.*?)[\'\"]>(.*)$', row[6], flags=re.IGNORECASE)
             if m is not None:
