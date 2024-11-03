c59ddb4ad8148737dac69e278174f8d1570069e7
mlo31415 <mlo@baskerville.org>
2024-11-02 20:06:50 -0400

    Allow single quotes in filenames

--- FanzineIndexPageEdit.py
+++ FanzineIndexPageEdit.py
@@ -2483,7 +2483,7 @@ class FanzineIndexPage(GridDataSource):
 
             # OK, it's an ordinary row
             insert+=f"\n<TR>"
-            insert+=f"\n<TD><a href='{row.Cells[0]}'>{UnicodeToHtmlEscapes(row.Cells[1])}</A></TD>\n"
+            insert+=f'\n<TD><a href="{row.Cells[0]}">{UnicodeToHtmlEscapes(row.Cells[1])}</A></TD>\n'
             for i, cell in enumerate(row.Cells[2:]):
                 if self.ColHeaders[i+2].lower() == "mailing":
                     insert+=f"<TD CLASS='left'>{self.ProcessAPALinks(cell)}</TD>\n"
